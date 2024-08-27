af5c0e8c34856180b01ff06225d0f182947c42f9
Justin Glover <jglover16@icloud.com>
2024-08-27 13:54:40 -0500

    final code v1

--- kanban.js
+++ kanban.js
@@ -1,21 +1,76 @@
 export default class Kanban{
+
     static getTasks(columnId){
-
-    }
-    static insertTask(columnId, content){
+        const data = read().find(column => {
+            return column.columnId == columnId;
+        });
         
+        return data.tasks;
     }
 
-    static updateTask(taskId, ){
+    static insertTask(columnId, content){
+        const data = read();
+        const column = data.find(column => {
+            return column.columnId == columnId;
+        });
+        const task = {
+            taskId: Math.floor(Math.random() * 100000),
+            content: content
+        };
 
+        column.tasks.push(task);
+        console.log(data);
+        save(data);
+
+        return task;
     }
 
-    static deleteTask(taskId, updatedInfo){
+    static updateTask(taskId, updatedInformation){
+        const data = read();
 
+        function findColumnTask(){
+            for(const column of data){
+                const task = column.tasks.find(item => {
+                    return item.taskId == taskId;
+                });
+    
+                if(task){
+                    return [task, column];
+                }
+            }
+        }
+        const [task, currentColumn] = findColumnTask();
+
+        const targetColumn = data.find(column => {
+            return column.columnId == updatedInformation.columnId;
+        });
+
+        task.content = updatedInformation.content;
+        currentColumn.tasks.splice(currentColumn.tasks.indexOf(task), 1);
+        targetColumn.tasks.push(task);
+
+        save(data);
+    }
+
+    static deleteTask(taskId){
+        const data = read();
+
+        for(const column of data){
+            const task = column.tasks.find(item => {
+                return item.taskId == taskId;
+            });
+
+            if(task){
+                column.tasks.splice(column.tasks.indexOf(task), 1);
+            }            
+        }
+
+        save(data);
     }
 
     static getAllTasks(){
         const data = read();
+        columnCount();
         return [data[0].tasks, data[1].tasks, data[2].tasks];
     }
 }
@@ -29,11 +84,47 @@ function read(){
             {columnId: 1, tasks: []}, 
             {columnId: 2, tasks: []}
         ];
-    } //if data is empty, then create dummy data
+    }
 
     return JSON.parse(data);
 }
 
-function save(){
-
+function save(data){
+    localStorage.setItem("data", JSON.stringify(data));
+    columnCount();
 }
+
+function columnCount(){
+    const data = read();
+
+    const todo = document.querySelector("span.todo");
+    todo.textContent = data[0].tasks.length;
+
+    const pending = document.querySelector("span.pending");
+    pending.textContent = data[1].tasks.length;
+
+    const completed = document.querySelector("span.completed");
+    completed.textContent = data[2].tasks.length;
+}
+
+
+// console.log(Kanban.getAllTasks());
+// console.log(Kanban.getTasks(1));
+
+
+// console.log(Kanban.getTasks(1));
+// console.log(Kanban.insertTask(0, "Record Kanban Lectures"));
+// console.log(Kanban.getTasks(1));
+
+
+// console.log(Kanban.getAllTasks());
+// Kanban.deleteTask(11822);
+// console.log(Kanban.getAllTasks());
+
+
+// console.log(Kanban.getAllTasks());
+// Kanban.updateTask(97522, {
+//     columnId: 2,
+//     content: "Record JavaScript Preview"
+// });
+// console.log(Kanban.getAllTasks());
