37457fe215efa75ba7faacb3f060fd7b86a6d46f
Justin Glover <jglover16@icloud.com>
2024-08-27 11:30:36 -0500

    added kanban file

---
+++ kanban.js
@@ -0,0 +1,28 @@
+export default class Kanban{
+    static getTasks(columnId){
+
+    }
+    static insertTask(columnId, content){
+        
+    }
+
+    static updateTask(taskId, ){
+
+    }
+
+    static deleteTask(taskId, updatedInfo){
+
+    }
+
+    static getAllTasks(){
+
+    }
+}
+
+function read(){
+
+}
+
+function save(){
+    
+}
